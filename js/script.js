c38e72ac8e62aa9bba2b1170863b246715729deb
Greta <greta.maria.brugnatti.2007@calvino.edu.it>
2025-01-14 18:31:17 +0100

    Modificato percorso file audio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function suona(strumento){
-    var crash1 = new Audio("../sound/crash1.mp3");
-    var crash2 = new Audio("../sound/crash2.mp3");
-    var hh = new Audio("../sound/hh.mp3");
-    var ride = new Audio("../sound/ride.mp3");
-    var highTom = new Audio("../sound/high-tom.mp3");
-    var midTom = new Audio("../sound/mid-tom.mp3");
-    var floor = new Audio("../sound/floor-tom.mp3");
-    var bass = new Audio("../sound/bass.mp3");
-    var snare = new Audio("../sound/snare.mp3");
+    var crash1 = new Audio("sound/crash1.mp3");
+    var crash2 = new Audio("sound/crash2.mp3");
+    var hh = new Audio("sound/hh.mp3");
+    var ride = new Audio("sound/ride.mp3");
+    var highTom = new Audio("sound/high-tom.mp3");
+    var midTom = new Audio("sound/mid-tom.mp3");
+    var floor = new Audio("sound/floor-tom.mp3");
+    var bass = new Audio("sound/bass.mp3");
+    var snare = new Audio("sound/snare.mp3");
 
     if(strumento == "crash1"){
         crash1.play();
